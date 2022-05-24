6b42760a81178c4e93df2a007293f65af903f090
Albert Lombarte <albert@lombarte.org>
2022-05-24 12:58:35 +0200

    Allow flexible configuration and other commands

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,28 @@
 #!/bin/sh
+# Reruns a KrakenD command every time the configuration changes
+# Pass KRAKEND_CONFIG with the filename you want to use as the configuration
+# Pass KRAKEND_COMMAND if you want a different command than run
 
-KRAKEND_CFG=${KRAKEND_CONFIG:-krakend.json}
+KRAKEND_CONFIG=${KRAKEND_CONFIG:-krakend.json}
+
+# Default KrakenD command
+if [[ -z "${KRAKEND_COMMAND}" ]]; then
+    KRAKEND_COMMAND="run -c /etc/krakend/${KRAKEND_CONFIG} -d"
+fi
+
+echo "Watching changes on files /etc/krakend/${KRAKEND_CONFIG}"
 
 cd /etc/krakend
-/usr/bin/reflex -g "$KRAKEND_CFG" -s  -- /usr/bin/krakend run -c /etc/krakend/${KRAKEND_CFG} -d
+
+# Run without flexible configuration
+if [[ -z "${FC_ENABLE}" ]]; then
+  /usr/bin/reflex -g "*.*" -s  -- /usr/bin/krakend $KRAKEND_COMMAND
+else
+# Run with flexible configuration values in ENV
+	FC_ENABLE=1 \
+    FC_SETTINGS="$FC_SETTINGS" \
+    FC_PARTIALS="$FC_PARTIALS" \
+    FC_TEMPLATES="$FC_TEMPLATES" \
+    FC_OUT="$FC_OUT" \
+    /usr/bin/reflex -g "*.*" -s  -- /usr/bin/krakend $KRAKEND_COMMAND
+fi
