fcb239d6b4584a0bc04e22f92fb4ec3130de868c
Daniel Ortiz <dortiz@krakend.io>
2022-09-14 15:28:28 +0200

    Monitor all the files and subdirs

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,4 +5,4 @@ FC_OUT=${FC_OUT:-out.json}
 echo "Watching changes on files /etc/krakend/"
 cd /etc/krakend
 # Watch all extensions and ignore FC_OUT to avoid infinte loop
-/usr/bin/reflex -g "*.*" -G "$FC_OUT" -s  -- /usr/bin/krakend $@
+/usr/bin/reflex -G "$FC_OUT" -s  -- /usr/bin/krakend $@
